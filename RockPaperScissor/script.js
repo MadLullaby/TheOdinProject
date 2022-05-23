ea0b89fcecd1686403d2095aba37674885104f5e
MadLullaby <xxmikasachan@gmail.com>
2022-05-23 11:36:43 +0200

    getting somewhere

--- RockPaperScissor/script.js
+++ RockPaperScissor/script.js
@@ -2,27 +2,56 @@
 We’ll use this function in the game to make the computer’s play. 
 Tip: use the console to make sure this is returning the expected output before moving to the next step!*/
 
+let playerScore = 0;
+let computerScore = 0;
+let result ="";
 const moves = ["rock", "paper", "scissor"];
 
 function computerPlay(){
+    
+    
     let move = moves[Math.floor(Math.random()*moves.length)];
-    console.log(move);
+    return move;;
 }
 computerPlay();
+
 
 
 /*Write a function that plays a single round of Rock Paper Scissors.
  The function should take two parameters - the playerSelection and computerSelection - 
  and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"*/
 
-function playerPlay(){
-    let message= prompt();
-
-    console.log(message);
-}
-playerPlay();
+ function round (playerSelection){
 
 
+computerSelection = computerPlay();
+playerSelection = prompt();
+
+if ((playerSelection == "rock" && computerSelection == "scissor") ||
+    (playerSelection == "paper" && computerSelection == "rock")   ||
+    (playerSelection == "scissor" && computerSelection == "paper"))
+    {
+        playerScore += 1;
+        return ("You Win! " + playerSelection + " beats " + computerSelection);
+       
+
+    }
+
+else if (playerSelection == computerSelection){
+        return "It\'s a tie. You both chose " + playerSelection ;
+    }
+
+else {
+        computerScore += 1;
+        return ("You Lose! " + computerSelection + " beats " + playerSelection);
+    }   
+    
+ }
+ 
+ for (let i = 0; i < 5; i++){
+
+ console.log(round());
+ }
 
 
 
