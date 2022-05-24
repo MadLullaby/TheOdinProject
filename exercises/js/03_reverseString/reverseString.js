69faf357a3c95a87b6cad1374e8b1c34746e5e27
MadLullaby <xxmikasachan@gmail.com>
2022-05-24 12:49:31 +0200

    3 done

--- exercises/js/03_reverseString/reverseString.js
+++ exercises/js/03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let splitString = string.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+
+    return joinArray;
 };
+
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
