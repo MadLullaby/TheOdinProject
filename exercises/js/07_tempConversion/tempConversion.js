3d729a8941ea95cbb617171610b82ddf3bd0b19f
MadLullaby <xxmikasachan@gmail.com>
2022-05-27 11:20:42 +0200

    7 done

--- exercises/js/07_tempConversion/tempConversion.js
+++ exercises/js/07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const ftoc = function() {
+const ftoc = function(fahr)
+{
+  
+  let celsius = (fahr - 32) * (5/9);
+
+  return Math.round(celsius * 10) / 10;
 
 };
 
-const ctof = function() {
+const ctof = function(cels)
+{
+
+    let fahrenheit = (cels * 1.8) + 32;
+
+    return Math.round(fahrenheit * 10) / 10;
 
 };
 
