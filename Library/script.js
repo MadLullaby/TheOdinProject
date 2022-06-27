306f4fa842d6db58c1a09df449f47c04ebfb3edf
MadLullaby <xxmikasachan@gmail.com>
2022-06-27 16:35:37 +0200

    not anymore

--- Library/script.js
+++ Library/script.js
@@ -7,7 +7,7 @@ let inputDiv = document.querySelector(".inputDiv")
 
 
 addBtn.addEventListener("click", ()=> {
-    addBook() //da togliere
+    // addBook() da togliere - mettere in line 50
     showInput()
     
     
@@ -18,16 +18,49 @@ function addBook()
 {
     let newDiv = document.createElement("div")
     newDiv.className= "book"
+    const pGroup = document.createElement("div")
+    const pAuthor = document.createElement("p")
+    const pTitle = document.createElement("p")
+    const pPages = document.createElement("p")
+    pGroup.appendChild(pAuthor)
+    pGroup.appendChild(pTitle)
+    pGroup.appendChild(pPages)
+    const btnGroup = document.createElement("div")
+    const readBtn = document.createElement("button")
+    const removeBtn = document.createElement("button")
+    btnGroup.appendChild(readBtn)
+    btnGroup.appendChild(removeBtn)
+    newDiv.appendChild(pGroup)
+    newDiv.appendChild(btnGroup)
+
+
     showBook.appendChild(newDiv)
 }
 
 function showInput()
 {    
     inputDiv.style.display = "block"
+    
 }
 
 let myLibrary=[]
-let
+const submitBtn = document.getElementById("confirm") // line 44
+submitBtn.addEventListener("click", () => {
+    getBookFromInput()
+    submitBtn.submit()  
+    addBook()
+    inputDiv.style.display = "none"
+})
+function getBookFromInput ()
+{
+const author = document.getElementById("name").value
+const title = document.getElementById("title").value
+const pages = document.getElementById("pages").value
+const read = document.getElementById("read").value
+const book = new bookConstructor()
+}
+
+
 
 function bookConstructor(author, title, pages, checkbox)
 {
@@ -43,10 +76,10 @@ function bookConstructor(author, title, pages, checkbox)
 
 function addBookToLibrary()
 {
-
+   
 }
 
 function createBook()
 {
-    const book = new bookConstructor()
+    
 }
