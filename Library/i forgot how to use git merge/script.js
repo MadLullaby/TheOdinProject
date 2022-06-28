5087a9eaacc7470bdc7473301290e9acb94f0929
MadLullaby <xxmikasachan@gmail.com>
2022-06-28 15:27:50 +0200

    i forgot how to merge

---
+++ Library/i forgot how to use git merge/script.js
@@ -0,0 +1,150 @@
+class Book 
+{
+    constructor(
+       author = "",
+        title = "",
+        pages = 0,
+        read = false 
+    )
+     {
+        this.author = author
+        this.title = title
+        this.pages = pages
+        this.read = read
+     }
+}
+
+class Library{
+    constructor()
+    {
+        this.books = []
+    }
+    addBook(newBook)
+    {
+        this.books.push(newBook)
+    }
+    removeBook(oldBook)
+    {
+        this.books = this.books.filter(book => book.oldBook !== oldBook)
+        
+    }
+    
+}
+const library = new Library()
+
+
+const updateBooksGrid = () => {
+    resetBooksGrid()
+    for (let book of library.books) {
+      createBookCard(book)
+    }
+  }
+  
+  const resetBooksGrid = () => {
+    showBook.innerHTML = ''
+  }
+  
+
+
+function addBook()
+{
+    
+    const newBook = getBookFromInput()
+    library.addBook(newBook)
+    updateBooksGrid()
+}
+function removeBook(e)
+{
+    const oldBook = e.target.parentNode.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+        '"',
+        ''
+      ) 
+    
+    library.removeBook(oldBook)
+    updateBooksGrid()
+}
+
+const addBtn = document.querySelector(".addButton");
+const showBook = document.querySelector(".bookContainer")
+//const book= document.querySelectorAll(".book")
+const container = document.querySelector(".container")
+const homepage = document.querySelector(".homepage")
+const inputDiv = document.querySelector(".inputDiv")
+
+
+addBtn.addEventListener("click", ()=> {
+   
+    showInput()
+    
+})
+
+
+
+
+// FUNZIONA 
+function createBookCard(book)
+{
+    const newDiv = document.createElement("div")
+    newDiv.className= "book"
+    const pGroup = document.createElement("div")
+    pGroup.className= "pGroup"
+    const author = document.createElement("p")
+    const title = document.createElement("p")
+    const pages = document.createElement("p")
+    pGroup.appendChild(author)
+    pGroup.appendChild(title)
+    pGroup.appendChild(pages)
+    const btnGroup = document.createElement("div")
+    btnGroup.className = "btnGroup"
+    const readBtn = document.createElement("button")
+    const removeBtn = document.createElement("button")
+    removeBtn.className = "removeBtn"
+    btnGroup.appendChild(readBtn)
+    btnGroup.appendChild(removeBtn)
+    newDiv.appendChild(pGroup)
+    newDiv.appendChild(btnGroup)
+    showBook.appendChild(newDiv)
+
+    author.textContent = book.author
+    title.textContent = book.title
+    pages.textContent = `${book.pages} pages`
+    removeBtn.textContent = "Remove"
+    if (book.read)
+    {
+        readBtn.textContent = "Read"
+        readBtn.className = "greenBtn"
+    } else
+    {
+        readBtn.textContent = "Not Read"
+        readBtn.className = "redBtn"
+    }
+    
+}
+
+
+
+function showInput()
+{    
+    inputDiv.style.display = "block"
+    
+}
+
+const submitBtn = document.getElementById("confirm") 
+
+submitBtn.addEventListener("click", () => {
+    addBook()
+
+    inputDiv.style.display = "none"
+    console.log(library)
+})
+
+function getBookFromInput ()
+{
+const author = document.getElementById("name").value
+const title = document.getElementById("title").value
+const pages = document.getElementById("pages").value
+const read = document.getElementById("read").value
+
+return new Book(author, title, pages, read)
+//mettere book in my library
+}
