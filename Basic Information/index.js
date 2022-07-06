87b44a51adfed25fb006f5db3a645ff446f047a5
MadLullaby <xxmikasachan@gmail.com>
2022-07-06 11:29:35 +0200

    final

--- Basic Information/index.js
+++ Basic Information/index.js
@@ -9,7 +9,8 @@ http.createServer((req, res) => {
       if (err) throw err;
       res.writeHead(200, {"Content-Type": "text/html"});
       res.write(data);
-      return res.end;
+      //return res.end; NO! endless loading page
+      res.end();
     });
   }
 
@@ -19,7 +20,7 @@ http.createServer((req, res) => {
       if (err) throw err;
       res.writeHead(200, {"Content-Type": "text-html"});
       res.write(data);
-      return res.end
+      res.end();
     });
   }
 
@@ -29,7 +30,7 @@ http.createServer((req, res) => {
       if (err) throw err;
       res.writeHead(200, {"Content-Type": "text/html"});
       res.write(data);
-      return res.end
+      res.end();
     });
   }
 
@@ -39,7 +40,7 @@ http.createServer((req, res) => {
       if (err) throw err;
       res.writeHead(200, {"Content-Type": "text/html"});
       res.write(data);
-      return res.end
+      res.end();
     })
   }
 }).listen(8080, ()=>{
