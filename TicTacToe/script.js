fd4ed7d7b7e4d755d759a112ec1eaebf94df93c5
MadLullaby <xxmikasachan@gmail.com>
2022-07-13 13:16:19 +0200

    help

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,2 +1,32 @@
-let gameArr = ["","","","","","","","",""];
-const winningArr = []
+
+const gameBoard = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""];
+
+
+
+const checkWinner = (fieldIndex) => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+]}
+})();
+
+const displayController = (() => {
+const gridEl = document.querySelectorAll(".gridEl")
+gridEl.forEach((field) => 
+    field.addEventListener("click", (e) => {
+    
+    }))
+})();
+
+
+const gameController = (() => {
+    
+    
+})();
